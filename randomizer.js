6b2481ed55d3ea6b8668ad134853ce3481d9ca9c
AJ <ajw53@NJIT.edu>
2023-01-11 16:34:56 -0500

    feature: v2! Now using an API for the episode data

---
+++ randomizer.js
@@ -0,0 +1,15 @@
+let episodes = []
+
+// Fetch data from Tvmaze API and store episodes in array
+fetch("https://api.tvmaze.com/shows/347/episodes").then(response => response.json()).then(jsonData => { episodes = jsonData })
+
+document.getElementById("butt").onclick=function(){
+
+    // Get a random episode
+    const episode = episodes[Math.floor(Math.random() * episodes.length)];
+
+    document.getElementById("season").value = episode.season;
+    document.getElementById("episode").value = episode.number;
+    document.getElementById("title").value = episode.name;
+    document.getElementById("desc").value = episode.summary;
+}
